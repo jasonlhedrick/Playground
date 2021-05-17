880c7f3904c80b782172e16ac1b1fd3c8614eb01
Jason Hedrick <jasonleehedrick@gmail.com>
2021-05-16 19:47:53 -0700

    Diving deeper with promises.

---
+++ async/promises1.js
@@ -0,0 +1,20 @@
+/*
+resolveAfter2Seconds is from Mozilla.
+Link here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+*/
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+}
+
+asyncCall();
