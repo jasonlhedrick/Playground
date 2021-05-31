145c50a005fb771f525b42815653190778a2c064
Jason Hedrick <jasonleehedrick@gmail.com>
2021-05-31 15:10:50 -0700

    Beginning deep dive into graphs and HTML layouts.

---
+++ datastructures/graphs/graphpractice1.js
@@ -0,0 +1,20 @@
+let graph = [];
+graph[0] = null;
+graph[1] = 1;
+graph[2] = 2;
+graph[3] = 3;
+
+let n = graph.length;
+let visited = [];
+
+function dfs(at) {
+    if (visited[at]) return;
+    visited[at] = true;
+    console.log(visited);
+    let neighbors = graph[at];
+    for (let i = 0; i < n; i++) {
+        dfs(i);
+    }
+}
+
+dfs(0);
