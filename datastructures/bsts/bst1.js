b0c934ee12fdd001fb30ac951989e2458cb1e083
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-05 23:51:24 -0700

    Built out the add, findMin, findMax, find and isPresent functions.

--- datastructures/bsts/bst1.js
+++ datastructures/bsts/bst1.js
@@ -18,37 +18,86 @@ class BST {
             this.root = new Node(data);
             return;
         }
-        else {
-            // Create a recursive function to search through looking for a proper empty space.
-            const searchTree = function(node) {
-                // If the data is less than the node.data we're going left.
-                if (data < node.data) {
-                    // If there isn't a node to the left we create a new node and add it there.
-                    if (node.left === null) {
-                        node.left = new Node(data);
-                    }
-                    // Else go down a node to the left and do the search again.
-                    else if (node.left !== null) {
-                        return searchTree(node.left);
-                    }
+    
+        // Create a recursive function to search through looking for a proper empty space.
+        const searchTree = function(node) {
+            // If the data is less than the node.data we're going left.
+            if (data < node.data) {
+                // If there isn't a node to the left we create a new node and add it there.
+                if (node.left === null) {
+                    node.left = new Node(data);
+                    return;
                 }
-                // If the data is greater than the node.data we're going right.
-                else if (data > node.data) {
-                    // If there isn't a node to the right than create a new node and add it there.
-                    if (node.right === null) {
-                        node.right = new Node(data);
-                    }
-                    // Else go down a node to the right and do the search again.
-                    else if (node.right !== null) {
-                        return searchTree(node.right);
-                    }
-                }
-                /* Neither data < node.data or data > node.data branch was called meaning
-                    This prevents data being added to the BST that's already part of it.
-                */
-                else return null;
+                // Else go down a node to the left and do the search again.
+                if (node.left !== null) return searchTree(node.left);
             }
-            return searchTree(node);
+            // If the data is greater than the node.data we're going right.
+            if (data > node.data) {
+                // If there isn't a node to the right than create a new node and add it there.
+                if (node.right === null) {
+                    node.right = new Node(data);
+                    return;
+                }
+                // Else go down a node to the right and do the search again.
+                if (node.right !== null) return searchTree(node.right);
+            }
+            /* Neither data < node.data or data > node.data branch was called meaning
+                This prevents data being added to the BST that's already part of it.
+            */
+            else return null;
+        }
+        return searchTree(node);
+    }
+
+    findMin() {
+        let current = this.root;
+
+        while(current.left !== null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    findMax() {
+        let current = this.root;
+
+        while(current.right !== null) {
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    find(data) {
+        let current = this.root;
+
+        while(current.data !== data) {
+            if (current === null) return null;
+            if (data < current.data) current = current.left;
+            else current = current.right;
+        }
+        return current;
+    }
+
+    isPresent(data) {
+        let current = this.root;
+        
+        while(current) { 
+            /***************
+                If the data matches up return ASAP
+                If we've run into a null dead end return ASAP.
+                If data is smaller than the current node go left.
+                Otherwise go right.
+            ****************/
+            if (data === current.data) return true;
+            if (current ===  null) return false;
+            if (data < current.data) current = current.left;
+            else current = current.right;
         }
     }
 }
+
+let myBST = new BST();
+
+myBST.add(1);
+console.log(myBST.root.data);
+console.log(myBST.isPresent(1));
