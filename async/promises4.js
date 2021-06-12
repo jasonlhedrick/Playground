e746135144fcc8f6e533e1ac81dd997840007a75
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-12 16:52:02 -0700

    Practicing an async await login method instead of chaining thens.

--- async/promises4.js
+++ async/promises4.js
@@ -7,25 +7,56 @@ const db = new sqlite.Database(dbPath, (err) => {
     else console.log(`Connected to database ${dbPath}`);
 })
 
-async function test() {
+async function test(password) {
     const selectAllUsers = new Promise((resolve, reject) => {
-        db.each('SELECT * FROM users', (err, rows) => {
+        db.all('SELECT * FROM users', (err, rows) => {
             if (err) reject(err);
             resolve(rows);
         })
     })
+
+    return await selectAllUsers;
+    /*
     const user = await selectAllUsers;
-    const checkPass = await hash.compareHash('hello world', user.hash);
+    const checkPass = await hash.compareHash(password, user.hash);
     if (checkPass === true) {
         return {user_id: user.user_id, email: user.email};
     }
+    return {errMessage: 'Username or password incorrect.'}
+    */
+}
 
+async function login(user) {
+    const selectUser = new Promise((resolve, reject) => {
+        db.each('SELECT * FROM users WHERE email=?', [user.email], (err, row) => {
+            if (err) reject(err);
+            resolve(row);
+        });
+    });
+    const checkUser = await selectUser;
+    const checkPass = await hash.compareHash(user.pass, checkUser.hash);
+    if (checkPass) return { user_id: checkUser.user_id, email: checkUser.email };
+    return { error: 'Username or password not matched.' };
 }
 
 const  runTest = async () => {
-    console.log(await test());
+    try {
+        const testLogin = await login({email: 'test@test.com', pass: '123456789'});
+        console.log(testLogin)
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+}
+
+async function addUser(user) {
+    user.pass = await hash.hashPass(user.pass);
+    return new Promise((resolve, reject) => {
+        db.run('INSERT INTO users(email, hash) VALUES(?,?)', [user.email, user.pass], (err, row) => {
+            if (err) reject(err);
+            resolve(row);
+        })
+    })
 }
 
 runTest();
-
-db.close();
